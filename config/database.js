6b98b7666d5db7e56e22158fb5c345f61bc2433b
David.Sells <david@persistentdesigns.com>
2014-03-28 22:02:40 -0400

    Initial checkin

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+	//'url' : 'your_database_here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://fred:fred@ds033069.mongolab.com:33069/persistentdesigns'
+
+};
